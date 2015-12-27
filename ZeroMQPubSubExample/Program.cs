2a98d2f0c1b759c9302c12e83abc26977dfdca2a
Cerem Cem ASLAN <ceremcem@ceremcem.net>
2015-12-27 03:27:01 +0200

    subscriber is ready for moving to another thread (backgroundworker)

--- ZeroMQPubSubExample/Program.cs
+++ ZeroMQPubSubExample/Program.cs
@@ -1,15 +1,36 @@
 ﻿using NetMQ;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ZeroMQPubSubExample
 {
+    class ActorSubscriber
+    {
+        BackgroundWorker sub_worker;
+
+        public ActorSubscriber()
+        {
+            sub_worker = new BackgroundWorker();
+            sub_worker.DoWork += Sub_worker_DoWork;
+            sub_worker.RunWorkerAsync();
+        }
+
+        private void Sub_worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (true)
+            {
+                Console.WriteLine("naber");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        
+    }
     class Program
     {
-
 
         private static double unix_timestamp(DateTime value)
         {
@@ -28,24 +49,27 @@ namespace ZeroMQPubSubExample
 
         static void Main(string[] args)
         {
-
+            ActorSubscriber a = new ActorSubscriber();
             using (var context = NetMQContext.Create())
             using (var pub = context.CreatePublisherSocket())
             using (var sub = context.CreateSubscriberSocket())
             {
                 pub.Connect("tcp://localhost:5012");
                 sub.Connect("tcp://localhost:5013");
+                sub.Subscribe(""); 
                 int i = 0; 
                 while(true)
                 {
                     
-                    System.Threading.Thread.Sleep(1000);
                     string timestamp = unix_timestamp_now().ToString().Replace(',', '.'); 
                     string raw_msg = "{ \"timestamp\": " + timestamp + ", \"msg_id\": \"MZ9YV."+ i++ +"\", \"sender\": [\"MZ9YV\", \"cKsNg\"], \"payload\": {\"PongMessage\": {\"text\": \"Hello ponger, this is pinger 1!\"}}}";
                     //byte[] bytes = Encoding.Default.GetBytes(raw_msg);
                     //raw_msg = Encoding.UTF8.GetString(bytes);
 
                     pub.SendFrame(raw_msg);
+                    Console.WriteLine("From Server: {0}", sub.ReceiveFrameString());
+                    System.Threading.Thread.Sleep(10000);
+
                     //break;
                 }
 
