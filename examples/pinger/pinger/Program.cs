1303ed509ba9ce7670912f8fce45356a5f22651b
cca <cca@aktos>
2015-12-30 15:27:08 +0200

    pinger example is added

---
+++ examples/pinger/pinger/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using aktos_dcs_cs; 
+
+namespace pinger
+{
+
+    class Pinger : Actor
+    {
+        public override void action()
+        {
+            while (true)
+            {
+                Console.WriteLine("Pinger is in action! ");
+                System.Threading.Thread.Sleep(2000);
+
+                string msg_ser = @"
+                    {""PongMessage"": 
+                        {""text"": ""this is proper message from csharp implementation""}
+                    }
+                "; 
+
+                send(msg_ser);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actor pinger = new Pinger();
+            Actor.wait_all(); 
+        }
+    }
+}
