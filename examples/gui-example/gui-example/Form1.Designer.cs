0e03ffffdb4eb0b8962004340a841a9a52c38725
cca <cca@aktos>
2016-01-14 01:39:57 +0200

    gui example is fully working

--- examples/gui-example/gui-example/Form1.Designer.cs
+++ examples/gui-example/gui-example/Form1.Designer.cs
@@ -31,22 +31,23 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(151, 36);
+            this.button1.Location = new System.Drawing.Point(12, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Send!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(41, 17);
+            this.label1.Location = new System.Drawing.Point(15, 115);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 1;
@@ -54,16 +55,26 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(151, 10);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(582, 20);
             this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Received Message:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(286, 205);
+            this.ClientSize = new System.Drawing.Size(636, 308);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -79,6 +90,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- examples/gui-example/gui-example/Form1.cs
+++ examples/gui-example/gui-example/Form1.cs
@@ -7,19 +7,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using aktos_dcs_cs; 
 
 namespace gui_example
 {
+    /**
+    run aktos-dcs/examples/ponger.py in order to communicate with another actor
+    **/
     public partial class Form1 : Form
     {
+        ExamplePinger comm = new ExamplePinger(); 
+
         public Form1()
         {
             InitializeComponent();
+            comm.event_PingMessage += handle_PingMessage; 
         }
 
+        private void handle_PingMessage(Dictionary<string, object> msg)
+        {
+            string received_text = (string)msg["text"];
+            if (label1.InvokeRequired)
+            {
+                label1.Invoke(new MethodInvoker(delegate { label1.Text = received_text; }));
+            }
+            // do any other work here
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-            label1.Text = textBox1.Text; 
+            comm.send_PongMessage(textBox1.Text); 
         }
+
     }
+    class ExamplePinger : Actor
+    {
+        public delegate void msg_callback(Dictionary<string, object> msg);
+        public event msg_callback event_PingMessage;
+
+        public void send_PongMessage(string msg_json)
+        {
+            string msg_ser = string.Format(@"
+                {{""PongMessage"": 
+                    {{""text"": ""{0}""}}
+                }}
+            ", msg_json);
+            send(msg_ser);
+        }
+
+        public override void action()
+        {
+            // do your background work here
+            while (true)
+            {
+                Console.WriteLine("Pinger is in action! ");
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+        public void handle_PingMessage(Dictionary<string, object> msg)
+        {
+            if (event_PingMessage != null) event_PingMessage(msg);
+        }
+
+    }
+
 }
